84f42c3238757645081533923fe917d6f9beb123
Hung Truong <hungtruongquoc@gmail.com>
2021-01-25 14:37:03 -0600

    Update HomeController.java

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,4 +1,19 @@
-package com.udacity.jwdnd.course1.cloudstorage.controller;
+ackage com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.form.LoginForm;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+@EnableWebSecurity
 public class HomeController {
+    @GetMapping()
+    public String homeView(Model model) {
+        return "home";
+    }
 }
