c9354781db0e60970e095e5517bfd38668426868
Hung Truong <hungtruongquoc@gmail.com>
2021-01-25 14:38:25 -0600

    Update SignUpController.java

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
@@ -2,6 +2,9 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
 @RequestMapping("/signup")
+@EnableWebSecurity
 public class SignUpController {
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(SignUpController.class);
 
     public SignUpController(UserService userService) {
         this.userService = userService;
@@ -26,7 +31,7 @@ public class SignUpController {
     @PostMapping()
     public String signupUser(@ModelAttribute User user, Model model) {
         String signupError = null;
-
+        logger.info("User info:", user);
         if (!userService.isUsernameAvailable(user.getUsername())) {
             signupError = "The username already exists.";
         }
@@ -36,11 +41,13 @@ public class SignUpController {
             if (rowsAdded < 0) {
                 signupError = "There was an error signing you up. Please try again.";
             }
+            logger.info("User created", rowsAdded);
         }
 
         if (signupError == null) {
             model.addAttribute("signupSuccess", true);
         } else {
+            logger.info("Error: " + signupError.toString());
             model.addAttribute("signupError", signupError);
         }
 
