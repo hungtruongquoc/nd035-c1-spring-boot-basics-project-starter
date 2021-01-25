492ea5eae9b0a0769f578d62adce3384c9f37f04
Hung Truong <hungtruongquoc@gmail.com>
2021-01-25 10:32:38 -0600

    20210125: Working on the sign up form

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.form.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping()
-    public String loginView() {
+    public String loginView(@ModelAttribute("user") LoginForm loginForm, Model model) {
+        String signInError = null;
+        model.addAttribute("signInError", signInError);
+        return "login";
+    }
+
+    @PostMapping()
+    public String loginUser(@ModelAttribute("user") LoginForm loginForm, Model model) {
+        String signInError = null;
+        model.addAttribute("signInError", signInError);
         return "login";
     }
 }
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/form/LoginForm.java
@@ -0,0 +1,14 @@
+package com.udacity.jwdnd.course1.cloudstorage.form;
+
+public class LoginForm {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
