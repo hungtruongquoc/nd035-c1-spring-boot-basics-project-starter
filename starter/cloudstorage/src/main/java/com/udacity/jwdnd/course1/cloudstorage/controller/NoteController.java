8d9a6ab7826dac4ece1986ce7cf52de244488694
Hung Truong <hungtruongquoc@gmail.com>
2021-02-17 08:36:57 -0600

    20210216: Refactored the controller for the working with note data

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -12,8 +12,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/notes")
@@ -24,45 +27,85 @@ public class NoteController {
 
     private static final Logger logger = LoggerFactory.getLogger(NoteController.class);
 
+    private HashMap<String, Object> createNote(NoteForm note) throws NoSuchMethodException, InvocationTargetException,
+            IllegalAccessException {
+        String noteDataOperationError = "Failed to create this note. Please try again later.";
+        boolean noteDataOperationSuccess = false;
+        Note targetNote;
+        HashMap<String, Object> result = new HashMap<>();
+
+        targetNote = new Note();
+        targetNote.setNoteTitle(note.getNoteTitle()).setNoteDescription(note.getNoteDescription());
+
+        if (1 == this.performDataOperation("createNote", targetNote)) {
+            noteDataOperationSuccess = true;
+            noteDataOperationError = null;
+        }
+
+        result.put("noteDataOperationError", noteDataOperationError);
+        result.put("noteDataOperationSuccess", noteDataOperationSuccess);
+        return result;
+    }
+
+    private HashMap<String, Object> updateNote(NoteForm note) throws NoSuchMethodException, InvocationTargetException,
+            IllegalAccessException {
+        String noteDataOperationError = "Failed to update provided note. Please try again later.";
+        boolean noteDataOperationSuccess = false;
+        Note targetNote;
+        HashMap<String, Object> result = new HashMap<>();
+
+        targetNote = this.noteService.getById(note.getNoteId());
+        targetNote.setNoteTitle(note.getNoteTitle()).setNoteDescription(note.getNoteDescription());
+
+        if (1 == this.performDataOperation("updateNote", targetNote)) {
+            noteDataOperationSuccess = true;
+            noteDataOperationError = null;
+        }
+
+        result.put("noteDataOperationError", noteDataOperationError);
+        result.put("noteDataOperationSuccess", noteDataOperationSuccess);
+        return result;
+    }
+
+    private Integer performDataOperation(String methodName, Note targetNote)
+            throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Method performDataManipulation = NoteService.class.getMethod(methodName, Note.class);
+        return (Integer) performDataManipulation.invoke(this.noteService, targetNote);
+    }
+
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @PostMapping()
     public ModelAndView createNote(Model model, @ModelAttribute("newNote") NoteForm note) {
-        String createNoteError = null;
-        String updateNoteError = null;
-        boolean createNoteSuccess = false;
-        boolean updateNoteSuccess = false;
 
-        if (note.isValid()) {
-            if (null == note.getNoteId()) {
-                Note newNote = new Note();
-                newNote.setNoteTitle(note.getNoteTitle()).setNoteDescription(note.getNoteDescription());
+        HashMap<String, Object> viewData = new HashMap<>();
 
-                if (1 == this.noteService.createNote(newNote)) {
-                    createNoteSuccess = true;
+        try {
+            if (note.isValid()) {
+                if (null == note.getNoteId()) {
+                    viewData = this.createNote(note);
                 } else {
-                    createNoteError = "Failed to create note. Please try again later.";
+                    viewData = this.updateNote(note);
                 }
             } else {
-                Note currentNote = this.noteService.getById(note.getNoteId());
-                currentNote.setNoteTitle(note.getNoteTitle());
-                currentNote.setNoteDescription(note.getNoteDescription());
-                if (1 == this.noteService.updateNote(currentNote)) {
-                    updateNoteSuccess = true;
-                } else {
-                    updateNoteError = "Failed to update the note. Please try again later.";
-                }
+                viewData.put("noteDataOperationError", "Input is invalid");
+                viewData.put("noteDataOperationSuccess", false);
             }
-        } else {
-            createNoteError = "Input is invalid";
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException invocationError) {
+            viewData.put("noteDataOperationError", "Failed to work with note. Please try again later");
+            viewData.put("noteDataOperationSuccess", false);
+            logger.error("Cannot perform either update/create method using method invocation.");
+            logger.error("Please make sure: the method does exist/the object is instantiated/the method is accessible");
+        } catch (Exception exception) {
+            viewData.put("noteDataOperationError", "Failed to work with note. Please try again later");
+            viewData.put("noteDataOperationSuccess", false);
+            logger.error("Cannot create or update the provided note. Maybe some database error.");
         }
 
-        model.addAttribute("createNoteError", createNoteError);
-        model.addAttribute("createNoteSuccess", createNoteSuccess);
-        model.addAttribute("updateNoteError", updateNoteError);
-        model.addAttribute("updateNoteSuccess", updateNoteSuccess);
+
+        model.addAllAttributes(viewData);
         return new ModelAndView("redirect:/home?active=notes", (ModelMap) model);
     }
 }
