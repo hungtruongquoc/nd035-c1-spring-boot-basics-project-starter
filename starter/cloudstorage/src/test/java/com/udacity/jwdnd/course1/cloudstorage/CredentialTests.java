b951ca14968cda1d8894f97906484d8136bb25a8
Hung Truong <hungtruongquoc@gmail.com>
2021-05-01 22:08:21 -0500

    20210501: Added new test case for delete and create credential

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialTests.java
@@ -4,14 +4,16 @@ import com.udacity.jwdnd.course1.cloudstorage.pageobject.CredentialPage;
 import com.udacity.jwdnd.course1.cloudstorage.pageobject.LoginPage;
 import com.udacity.jwdnd.course1.cloudstorage.pageobject.NotePage;
 import com.udacity.jwdnd.course1.cloudstorage.pageobject.SignUpPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.UUID;
 
-public class CredentialTests extends CloudStorageApplicationTests{
+public class CredentialTests extends CloudStorageApplicationTests {
     private CredentialPage page;
     private LoginPage loginPage;
     private SignUpPage signUpPage;
@@ -51,6 +53,37 @@ public class CredentialTests extends CloudStorageApplicationTests{
         waitDriver.until(ExpectedConditions.elementToBeClickable(page.getSaveButton()));
         page.saveCredential();
         waitDriver.until(ExpectedConditions.elementToBeClickable(page.getLogoutButton()));
+        waitDriver.until(ExpectedConditions.textToBePresentInElement(page.getSuccessAlert(),
+                "New credential created"));
+        Assertions.assertTrue(page.getSuccessAlert().isDisplayed());
+        Assertions.assertTrue(page.getItemRow(driver, 1).isDisplayed());
+        Assertions.assertNotEquals(page.getTableItemPassword(driver, 1).getText(), "testpass");
     }
 
+    @Test
+    public void deleteACredential() {
+        page.showAddDialog();
+        waitDriver.until(ExpectedConditions.visibilityOfAllElements(page.getAddDialog()));
+        Assertions.assertTrue(page.isCredentialModalDisplayed());
+        page.setCredentialInputs("https://delete.com", "abc", "deletepass");
+        waitDriver.until(ExpectedConditions.elementToBeClickable(page.getSaveButton()));
+        page.saveCredential();
+        WebElement itemDelete = page.getTableItemDeleteButton(driver, 1);
+        waitDriver.until(ExpectedConditions.elementToBeClickable(itemDelete));
+        Assertions.assertTrue(page.getItemRow(driver, 1).isDisplayed());
+        Assertions.assertEquals(page.getTableItemUrl(driver, 1).getText(), "https://delete.com");
+        itemDelete.click();
+        WebElement confirmDelete = page.getDeleteConfirmButton(driver);
+        waitDriver.until(ExpectedConditions.elementToBeClickable(confirmDelete));
+        confirmDelete.click();
+        waitDriver.until(ExpectedConditions.visibilityOfAllElements(page.getSuccessAlert()));
+        Assertions.assertEquals(page.getSuccessAlert().getText(), "The provided credential was deleted successfully.");
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        page.logout();
+        super.afterEach();
+    }
 }
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageobject/CredentialPage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageobject/CredentialPage.java
@@ -34,7 +34,7 @@ public class CredentialPage extends HomePage{
         return saveCredentialButton;
     }
 
-    @FindBy(id = "credential-submit")
+    @FindBy(css = "#credentialModal #credential-submit")
     private WebElement saveCredentialButton;
 
     public WebElement getAddDialog() {
@@ -43,6 +43,13 @@ public class CredentialPage extends HomePage{
 
     @FindBy(id = "credentialModal")
     private WebElement addDialog;
+
+    public WebElement getSuccessAlert() {
+        return successAlert;
+    }
+
+    @FindBy(css = "#nav-credentials div.alert.alert-success")
+    private WebElement successAlert;
 
     public CredentialPage(WebDriver driver) {
         super(driver);
@@ -86,5 +93,28 @@ public class CredentialPage extends HomePage{
 
     public void saveCredential() {
         getSaveButton().click();
+    }
+
+    public WebElement getItemRow(WebDriver driver, Integer itemId) {
+        return findElementByCss(driver, "tr.credential-item[data-id='" + itemId.toString() + "']");
+    }
+
+    public WebElement getTableItemPassword(WebDriver driver, Integer itemId) {
+        return findElementByCss(driver, "tr.credential-item[data-id='" + itemId.toString()
+                + "'] td.item-password");
+    }
+
+    public WebElement getTableItemDeleteButton(WebDriver driver, Integer itemId) {
+        return findElementByCss(driver, "tr.credential-item[data-id='" + itemId.toString()
+                + "'] td a.credential-delete");
+    }
+
+    public WebElement getTableItemUrl(WebDriver driver, Integer itemId) {
+        return findElementByCss(driver, "tr.credential-item[data-id='" + itemId.toString()
+                + "'] th.credential-url");
+    }
+
+    public WebElement getDeleteConfirmButton(WebDriver driver) {
+        return findElementByCss(driver, "#deleteCredentialModal button.confirm-delete-item");
     }
 }
