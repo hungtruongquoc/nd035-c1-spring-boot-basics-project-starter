91792aa75b2a93189a278a3218cdb0db8a45fecb
Hung Truong <hungtruongquoc@gmail.com>
2021-05-06 00:57:16 -0500

    20210506: Fixed the handler of uploading a large file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/ControllerAdvice/FileUploadExceptionAdvice.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/ControllerAdvice/FileUploadExceptionAdvice.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 @ControllerAdvice
 public class FileUploadExceptionAdvice {
@@ -19,6 +17,6 @@ public class FileUploadExceptionAdvice {
                                                HttpServletResponse response) {
         ModelMap newModelMap = new ModelMap();
         newModelMap.addAttribute("errorMessage", "File too large!");
-        return new ModelAndView("redirect:/home", (Map<String, ?>) newModelMap);
+        return new ModelAndView("home", (ModelMap) newModelMap);
     }
 }
